7c40bb4556105e8d512f0043e6b16ad5413d487b
AbdelrahmanElatrash <92904041+AbdelrahmanElatrash@users.noreply.github.com>
2023-04-06 12:36:30 +0300

    Update test_series.py

--- test/test_series.py
+++ test/test_series.py
@@ -20,37 +20,37 @@ def test_3():
     expected=1
     assert actual == expected
 
-def test_3():
+def test_4():
     actual=fibonacci(3)
     expected=2
     assert actual == expected
 
-def test_4():
+def test_5():
     actual=lucas(0)
     expected=2
     assert actual == expected
 
-def test_5():
+def test_6():
     actual=lucas(2)
     expected=3
     assert actual == expected
 
-def test_6():
+def test_7():
     actual=lucas(4)
     expected=7
     assert actual == expected
 
-def test_7():
+def test_8():
     actual=sum_series(0)
     expected=0
     assert actual == expected
 
-def test_8():
+def test_9():
     actual=sum_series(3)
     expected=2
     assert actual == expected
 
-def test_8():
+def test_10():
     actual=sum_series(4,2,1)
     expected=7
     assert actual == expected
